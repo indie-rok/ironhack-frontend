1b4320d5f36aefb42e7c2346e4068ec1c62d796d
Emmanuel Orozco <indi3.rok@gmail.com>
2020-01-17 11:17:38 -0600

    added inital files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import countries from "./data/countries";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
